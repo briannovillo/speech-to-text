8eb1bb17ec2a0fa77f34611f7878f7186e085f0d
briannovillo <brian@briannovillo.com.ar>
2019-02-15 17:03:08 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+## Speech to text POC using Python + PocketSphinx
+
+- For more info see: http://blog.eonidi.com/index.php/2018/01/24/reconocimiento-de-voz-tiempo-real-en-idioma-espanol-con-pocketsphinx/
+
---
+++ install.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+# Install libraries
+sudo apt install autoconf libtool automake bison python-dev swig libpulse-dev
+
+# Clone repos
+mkdir sphinx
+cd sphinx
+git clone https://github.com/cmusphinx/sphinxbase
+git clone https://github.com/cmusphinx/pocketsphinx
+
+# Install sphinxbase
+cd sphinxbase
+./autogen.sh
+make
+sudo make install
+
+# Install pocketsphinx
+cd ../pocketsphinx
+./autogen.sh
+make
+sudo make install
+
+# Install sound library for prevent alsa error installing pocketsphinx
+sudo apt-get install libasound2-dev
+
+# Install pocketsphinx interface for Python
+sudo pip install --upgrade pip setuptools wheel
+sudo pip install --upgrade pocketsphinx
+
+# Ver como llenar variable
+# python -c "from pocketsphinx import get_model_path; print(get_model_path())"
+
+# Hacer copia usando la variable
+#sudo cp es-20k.lm.bin /usr/local/lib/python2.7/dist-packages/pocketsphinx/model
+#sudo cp -R es-es/ /usr/local/lib/python2.7/dist-packages/pocketsphinx/model
+#sudo cp es.dict /usr/local/lib/python2.7/dist-packages/pocketsphinx/model
+
+# Run speech to text
+#python speech.py
